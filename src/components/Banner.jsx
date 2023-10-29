9821543108800f4a0e66b778deb9a5c2fee50ffa
imran <alimrancv870@gmail.com>
2023-10-30 00:23:17 +0600

    update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,4 @@
 import {React, useState} from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
 import Snowfall from 'react-snowfall';
 import herosociallogo1 from '../assets/img/etherscan.svg';
 import herosociallogo2 from '../assets/img/dextools.svg';
@@ -23,31 +22,27 @@ export default function Banner(){
     return(
         <div className="banner">
             <div className="snowfall-container">
-                <Snowfall snowflakeCount={300} />
+                <Snowfall snowflakeCount={300}/>
             </div>
-            <Container>
-                <Row className='text-center'>
-                    <Col>
-                        <div className="banner-content">
-                            <a href="/" className='boxed__btn'>BUY NARUTO</a>
-                            <p>BUY NARUTO Embark on an epic journey with Naruto <span>ナルト</span>, the ultimate meme project set to take the Ethereum blockchain by storm! It’s a global movement that promises to bring together cultures and communities from every corner of the world. In the bustling metropolis of the Ethereum blockchain, our skilled ninja developers have crafted a world where creativity and innovation reign supreme. So, gear up, join our fantastic community, and become a part of a story guaranteed to make history! Are you ready to unleash your inner ninja and start raids for Konoha? </p>
-                            <div className="social__links">
-                                <a href="/"><img src={herosociallogo1} alt="" />Etherscan</a>
-                                <a href="/"><img src={herosociallogo2} alt="" />Dextools</a>
-                                <a href="/"><img src={herosociallogo3} alt="" />Telegram</a>
-                                <a href="/"><img src={herosociallogo4} alt="" /> Twitter</a>
-                            </div>
+            <div className='container'>
+                <div className="banner-content">
+                    <a href="/" className='boxed__btn'>BUY NARUTO</a>
+                    <p>BUY NARUTO Embark on an epic journey with Naruto <span>ナルト</span>, the ultimate meme project set to take the Ethereum blockchain by storm! It’s a global movement that promises to bring together cultures and communities from every corner of the world. In the bustling metropolis of the Ethereum blockchain, our skilled ninja developers have crafted a world where creativity and innovation reign supreme. So, gear up, join our fantastic community, and become a part of a story guaranteed to make history! Are you ready to unleash your inner ninja and start raids for Konoha? </p>
+                    <div className="social__links">
+                        <a href="/"><img src={herosociallogo1} alt="" />Etherscan</a>
+                        <a href="/"><img src={herosociallogo2} alt="" />Dextools</a>
+                        <a href="/"><img src={herosociallogo3} alt="" />Telegram</a>
+                        <a href="/"><img src={herosociallogo4} alt="" /> Twitter</a>
+                    </div>
 
-                            <div className="copytoclipboard">
-                                <input type="text"  value={textToCopy}
-                                    onChange={(e) => setTextToCopy(e.target.value)}/>
+                    <div className="copytoclipboard">
+                        <input type="text"  value={textToCopy}
+                            onChange={(e) => setTextToCopy(e.target.value)}/>
 
-                                <button onClick={handleCopyToClipboard}> {isCopied && <span className='coppied'><FaRegClone /></span>} <FaRegClone /></button>
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
+                        <button onClick={handleCopyToClipboard}> {isCopied && <span className='coppied'><FaRegClone /></span>} <FaRegClone /></button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,14 @@
 import { React } from 'react'
-import {Container, Row, Col} from 'react-bootstrap';
 import logo from '../assets/img/naruto-site-logo.svg';
 
 export default function Header(){
     return(
         <header className="heading">
-            <Container>
-                <Row>
-                    <Col className='text-center'>
-                        <a href="/" className='site__logo'>
-                            <img src={logo} alt="" /> 
-                        </a>  
-                    </Col>
-                </Row>
-            </Container>
+            <div className='container'>
+                <a href="/" className='site__logo'>
+                    <img src={logo} alt="" /> 
+                </a>  
+            </div>
         </header>
     );
 

0f78cd016a1c8e8d758d9cb8684dd152cea38247
imran <alimrancv870@gmail.com>
2023-10-30 10:37:28 +0600

    finalupdate

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,8 @@
 import {React, useState} from 'react';
 import Snowfall from 'react-snowfall';
-import herosociallogo1 from '../assets/img/etherscan.svg';
+import herosociallogo1 from '../assets/img/etherscan.png';
 import herosociallogo2 from '../assets/img/dextools.svg';
-import herosociallogo3 from '../assets/img/telegram.svg';
+import herosociallogo3 from '../assets/img/telegram.png';
 import herosociallogo4 from '../assets/img/x_twitter.svg';
 import { FaRegClone } from "react-icons/fa";
 
@@ -27,7 +27,7 @@ export default function Banner(){
             <div className='container'>
                 <div className="banner-content">
                     <a href="/" className='boxed__btn'>BUY NARUTO</a>
-                    <p>BUY NARUTO Embark on an epic journey with Naruto <span>ナルト</span>, the ultimate meme project set to take the Ethereum blockchain by storm! It’s a global movement that promises to bring together cultures and communities from every corner of the world. In the bustling metropolis of the Ethereum blockchain, our skilled ninja developers have crafted a world where creativity and innovation reign supreme. So, gear up, join our fantastic community, and become a part of a story guaranteed to make history! Are you ready to unleash your inner ninja and start raids for Konoha? </p>
+                    <p>Embark on an epic journey with Naruto <span>ナルト</span>, the ultimate meme project set to take the Ethereum blockchain by storm! It’s a global movement that promises to bring together cultures and communities from every corner of the world. In the bustling metropolis of the Ethereum blockchain, our skilled ninja developers have crafted a world where creativity and innovation reign supreme. So, gear up, join our fantastic community, and become a part of a story guaranteed to make history! Are you ready to unleash your inner ninja and start raids for Konoha? </p>
                     <div className="social__links">
                         <a href="/"><img src={herosociallogo1} alt="" />Etherscan</a>
                         <a href="/"><img src={herosociallogo2} alt="" />Dextools</a>
@@ -36,7 +36,7 @@ export default function Banner(){
                     </div>
 
                     <div className="copytoclipboard">
-                        <input type="text"  value={textToCopy}
+                        <input readOnly type="text"  value={textToCopy}
                             onChange={(e) => setTextToCopy(e.target.value)}/>
 
                         <button onClick={handleCopyToClipboard}> {isCopied && <span className='coppied'><FaRegClone /></span>} <FaRegClone /></button>
